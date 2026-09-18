b314c9e8dc7abae8bef4cb45dc6b651f48233e8e
Denis H <XoMA32@yandex.ru>
2026-09-18 23:09:28 +0300

    add filter

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -24,15 +24,17 @@ console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
 const filter = (array, callback) => {
-    const result = [];
+    const result = []
 
-    for (let i = 0; i < array.length; i++) {
-        if (callback(array[i], i)) {
-            result.push(array[i]);
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index]
+
+        if (callback(element, index)) {
+            result.push(element)
         }
     }
 
-    return result;
-};
+    return result
+}
 
 
